3e9642f13082852eadee61a171f51e4a5656d9ba
AokoMercyline <aokomercyline34@gmail.com>
2020-10-25 23:22:26 +0300

    install crispy form for styling registration form

--- awwards/settings.py
+++ awwards/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'awwa',
     'users',
+    'crispy_forms',
     
 ]
 
@@ -132,4 +133,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
