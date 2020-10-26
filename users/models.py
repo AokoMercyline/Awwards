e1856fb199f7191113adc4a06c9063c5ecb0e1e6
AokoMercyline <aokomercyline34@gmail.com>
2020-10-26 07:45:29 +0300

    registered profile for user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from  django.auth.models import User
-
+from  django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class Profile(models.Model):
     user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
